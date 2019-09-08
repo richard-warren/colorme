88f0890268f5c7208b1f535d952ef3ad7efd97e0
rwarren2163 <richardwarren2163@gmail.com>
2019-09-07 22:38:26 -0400

    better figure spacing

--- colorme_demo.m
+++ colorme_demo.m
@@ -1,8 +1,10 @@
+close all
 numOffsets = 6;
-colors = 4;
+colors = 3;
 
+figHgt = 800/numOffsets;
 offsets = linspace(0,1,numOffsets+1);
-for i = 1:(length(offsets)-1)
+for i = 1:(length(offsets)-1)    
     colorme(colors, 'offset', offsets(i), 'showSamples', true, 'bgColor', 'black');
-    set(gcf, 'position', [0 (i-1)*150 400 150])
+    set(gcf, 'position', [0 (i-1)*figHgt 400 figHgt])
 end
